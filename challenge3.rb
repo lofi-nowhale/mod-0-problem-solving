4b547de46cb95fd92a693187151bd659437a01b7
Noelle Hemphill <noelle.hemphill@gmail.com>
2023-06-02 13:32:40 -0600

    Add overall goal + pseudo code

--- challenge3.rb
+++ challenge3.rb
@@ -1,5 +1,6 @@
 #Write a method or function that removes all instances of the letter s in a string. The method or function should accept a string as an argument and return the same string with every instance of the letter s removed.
-
+# overall goal - define a method to remove the letter s from your string. Method will need to accept the string as an argument 
+# def method (string) as the argument use .delete method and specify letter ("s") -> print the return!
 
 
 def remove_letter(string)
